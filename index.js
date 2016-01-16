8c3029b709007011dd85752a7f34afa4349b2fc7
spacegeek224 <marisusisme@gmail.com>
2016-01-16 16:31:46 +0000

    asdf

--- index.js
+++ index.js
@@ -4,5 +4,14 @@ var log = require('util').log;
 module.exports = {
   error: function(s) {
     log(String(s).red.bold);
+  },
+  warn: function(s) {
+    log(String(s).yellow.bold);
+  },
+  info: function(s) {
+    log(String(s).blue.bold);
+  },
+  ok: function(s) {
+    log(String(s).green.bold);
   }
 }
